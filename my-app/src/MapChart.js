6b64f328e57c7f69d586d13d181a735ae40234f9
AndresM77 <andres@triplean.com>
2019-10-09 19:52:43 -0400

    Changed Tooltip

--- my-app/src/MapChart.js
+++ my-app/src/MapChart.js
@@ -3,21 +3,21 @@ import { ComposableMap, Geographies, Geography, Marker } from "react-simple-maps
 import "./App.css"
 
 
-const geoUrl = 
+const geoUrl =
   "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
 
 const MapChart = ({ setTooltipContent }) => {  return (
     <div>
-      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>      
+      <ComposableMap data-tip="" projectionConfig={{ scale: 200 }}>
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
-            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo} 
+            geographies.map(geo => <Geography key={geo.rsmKey} geography={geo}
             />)
           }
         </Geographies>
         <Marker coordinates={[-74.006, 40.7128]} onMouseEnter={() => {
 
-              setTooltipContent("There is a point here");
+              setTooltipContent("Temperature: 33C");
             }}
             onMouseLeave={() => {
               setTooltipContent("");
