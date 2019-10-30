e06b77a61828c3c1387b1cc719643fd29394cf99
Ethan Astrachan <ethan.astrachan@duke.edu>
2019-10-29 22:49:50 -0400

    Only displays points with selected category and year

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,23 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import ReactTooltip from "react-tooltip";
 import App from './App'
 
 import "./index.css";
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import MapChart from "./components/MapChart";
-
-// function App() {
-//   const [content, setContent] = useState("");
-//   return (
-//     <div>
-//       <Navbar />
-//       <MapChart setTooltipContent={setContent} />
-//       <ReactTooltip>{content}</ReactTooltip>
-//     </div>
-//   );
-// }
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
