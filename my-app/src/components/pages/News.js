76ccba405f72b61d0648535fa638816b983880ab
Andres Montoya-Aristizabal <andres.montoyaaristizabal@duke.edu>
2019-11-05 18:12:52 -0500

    resolved bug in news

--- my-app/src/components/pages/News.js
+++ my-app/src/components/pages/News.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import NewsData from './item/newsdata';
 
 class News extends React.Component {
-
+/*
   componentDidMount() {
       fetch('https://newsapi.org/v2/everything?q=climate&apiKey=7f25e9ec7b7b4ac181855d9cd830bfab')
       .then(response => response.json())
-      .then(news => this.setState({}){
+      .then(news => this.setState({
+        news:
+          news.
+      }){
         let news = data.results.map((info) => {
           return(
             <div key={info.articles}>
@@ -17,10 +20,11 @@ class News extends React.Component {
         console.log("state", this.state.news);
       })
   }
+  */
   render() {
     return (
       <div>
-        {this.state.news}
+        //{this.state.news}
       </div>
     );
   }
