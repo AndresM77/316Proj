3895f25777949e8718ce85de189a0176db134070
Frank Geng <jianchao.geng@duke.edu>
2019-11-19 20:42:40 -0500

    updated database schema

--- backend/init.sql
+++ backend/init.sql
@@ -1,44 +1,28 @@
-CREATE TABLE Categories (
-	CID uuid PRIMARY KEY NOT NULL,
-	name VARCHAR(32) NOT NULL
-);
-
-CREATE TABLE Locations (
-	LID uuid PRIMARY KEY NOT NULL,
-	lat DECIMAL(10,8),
-	lng DECIMAL(10,8),
-	city TEXT,
-	state TEXT,
-	country TEXT
-);
-
-CREATE TABLE Nonprofits (
-	OID uuid PRIMARY KEY NOT NULL,
-	name VARCHAR(32) NOT NULL
-	description VARCHAR(64) NOT NULL,
-	CID uuid REFERENCES Categories(CID) NOT NULL
-);
-
 CREATE TABLE Air (
 	DPID uuid PRIMARY KEY,
-	CID uuid  REFERENCES Categories(CID) NOT NULL,
+	lat DECIMAL(10,8),
+	lng DECIMAL(10,8),
+	time DATE NOT NULL, 
+	country char(3),
 	quality FLOAT NOT NULL
 );
 
 CREATE TABLE Temperature (
 	DPID uuid PRIMARY KEY,
-	CID uuid REFERENCES Categories(CID) NOT NULL,
+	time DATE NOT NULL, 
+	country char(3) NOT NULL,
 	temperature FLOAT NOT NULL
 );
 
 CREATE TABLE Rain (
 	DPID uuid PRIMARY KEY,
-	CID uuid REFERENCES Categories(CID) NOT NULL,
+	time DATE NOT NULL, 
+	country char(3) NOT NULL,
 	rainfall FLOAT NOT NULL
-	);
+);
 
 CREATE TABLE Users (
-	username VARCHAR(32) PRIMARY KEY NOT NULL,
+	username VARCHAR(32) PRIMARY KEY NOT NULL, 
 	email VARCHAR(64) NOT NULL UNIQUE,
 	first_name VARCHAR(32) NOT  NULL,
 	last_name VARCHAR(32) NOT NULL
@@ -49,8 +33,9 @@ CREATE TABLE Favorites (
 	location uuid REFERENCES Locations(LID)
 );
 
-CREATE TABLE DataPoints (
-	DPID uuid PRIMARY KEY NOT NULL,
-	time DATE NOT NULL, 
-	LID uuid NOT NULL REFERENCES Locations(LID)
+CREATE TABLE Nonprofits (
+	OID uuid PRIMARY KEY NOT NULL,
+	name VARCHAR(32) NOT NULL
+	description VARCHAR(64) NOT NULL
+	-- CID uuid REFERENCES Categories(CID) NOT NULL
 );
