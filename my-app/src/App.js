878a17ed75d3aef17c9fc157c9ba7d9a4b4789a6
Adam Gonen <adam.gonen123@gmail.com>
2019-11-27 18:32:54 -0700

    sign up works, login is mocked working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,8 @@ import News from './components/pages/News';
 import Donate from './components/pages/Donate';
 import NotFoundPage from './components/pages/NotFoundPage';
 import Contacts from './components/pages/Contacts';
+import SignUp from './components/pages/Signup';
+import Login from './components/pages/Login';
 
 class App extends Component{
   render() {
@@ -22,6 +24,8 @@ class App extends Component{
           <Route path="/news" component={News}/>
           <Route path="/contacts" component={Contacts}/>
           <Route path="/donate" component={Donate}/>
+          <Route path="/signup" component={SignUp}/>
+          <Route path="/login" component={Login}/>
           <Route path="/404" component={NotFoundPage}/>
         </Switch>
         <footer>Made with 🌎 by Adam, Ryan, Joe, Andres, Ethan, and Frank</footer>
---
+++ my-app/src/components/pages/Signup.js
@@ -0,0 +1,252 @@
+import React from 'react';
+import { useForm, useField } from "react-form";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
+
+
+async function sendToServer(values) {
+    try {
+        await fetch("/api/v1/users", {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify(values),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    } catch (e) {
+        console.error(e)
+    }
+    return values;
+  }
+
+async function validateName(name, instance) {
+    if(!name) {
+        return "A name is required";
+    }
+
+    if(name.length > 32) {
+        return "Name is too long";
+    }
+
+    return false;
+}
+
+async function validateUsername(name, instance) {
+    if(!name) {
+        return "A username is required";
+    }
+
+    if(name.length > 32) {
+        return "Name is too long";
+    }
+
+    try {
+        const result = await fetch("/api/v1/users/validate/username", {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({username: name}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const json = await result.json()
+
+        if(!json.result) {
+            return "That username is taken.";
+        }
+    } catch (e) {
+        console.error(e)
+    }
+
+    return false;
+}
+
+async function validateEmail(name, instance) {
+    if(!name) {
+        return "An email is required";
+    }
+
+    if(name.length > 32) {
+        return "Name is too long";
+    }
+
+    try {
+        const result = await fetch("/api/v1/users/validate/username", {
+            method: "POST",
+            mode: "cors",
+            body: JSON.stringify({username: name}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const json = await result.json()
+
+        if(!json.result) {
+            return "That email is taken.";
+        }
+    } catch (e) {
+        console.error(e)
+    }
+
+    return false;
+}
+
+
+function FirstNameField() {
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField("firstName", {
+        validate: validateName
+    });
+
+    return (
+        <>
+            <input {...getInputProps()} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    )
+}
+
+function LastNameField() {
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField("lastName", {
+        validate: validateName
+    });
+
+    return (
+        <>
+            <input {...getInputProps()} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    )
+}
+
+function UsernameField() {
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField("username", {
+        validate: validateUsername
+    });
+
+    return (
+        <>
+            <input {...getInputProps()} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    )
+}
+
+function PasswordField() {
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField("password", {
+        validate: validateName
+    });
+
+    return (
+        <>
+            <input {...getInputProps()} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    )
+}
+
+function EmailField() {
+    const {
+        meta: { error, isTouched, isValidating },
+        getInputProps
+    } = useField("email", {
+        validate: validateEmail
+    });
+
+    return (
+        <>
+            <input {...getInputProps()} />{" "}
+            {isValidating ? (
+                <em>Validating...</em>
+            ) : isTouched && error ? (
+                <em>{error}</em>
+            ) : null}
+        </>
+    )
+}
+
+const SignUp = () => {
+        const {
+            Form,
+            meta: { isSubmitting, canSubmit}
+        } = useForm({
+            onSubmit: async (values, instance) => {
+                await sendToServer(values);
+                await cookies.set("climateAction", values.username);
+                window.location.replace("/");
+            }
+        })
+
+        return (
+            <div>
+                <div><h2 className="titleItem">Sign Up</h2></div>
+                <Form>
+                    <div>
+                        <label>
+                            First Name: <FirstNameField />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Last Name: <LastNameField />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Username: <UsernameField />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Password: <PasswordField />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Email: <EmailField />
+                        </label>
+                    </div>
+                    <div>
+                        <button type="submit" disabled={!canSubmit}>
+                        Submit
+                        </button>
+                    </div>
+
+                    <div>
+                        <em>{isSubmitting ? "Submitting..." : null}</em>
+                    </div>
+                </Form>
+            </div>
+        )
+    }
+
+export default SignUp;
