62198c9d58fe8f717effe1cc764e1f80ad474a9e
Andres Montoya-Aristizabal <andres.montoyaaristizabal@duke.edu>
2019-12-08 15:19:21 -0500

    updated tables

--- backend/init.sql
+++ backend/init.sql
@@ -3,33 +3,33 @@ CREATE TABLE Air (
 	lat DECIMAL(10,8),
 	lng DECIMAL(10,8),
 	time DATE NOT NULL,
-	country char(3) NOT NULL,
+	countryID char(3) REFERENCES Country(countryID) NOT NULL,
 	quality FLOAT NOT NULL
 );
 
 CREATE TABLE Temperature (
 	DPID uuid PRIMARY KEY,
 	time DATE NOT NULL,
-	country char(3) NOT NULL,
+	countryID char(3) REFERENCES Country(countryID) NOT NULL,
 	temperature FLOAT NOT NULL
 );
 
 CREATE TABLE Rain (
 	DPID uuid PRIMARY KEY,
 	time DATE NOT NULL,
-	country char(3) NOT NULL,
+	countryID char(3) REFERENCES Country(countryID) NOT NULL,
 	rainfall FLOAT NOT NULL
 );
 
 CREATE TABLE Country (
-	country char(3) PRIMARY KEY,
-	name VARCHAR(32) NOT NULL
+	name VARCHAR(32) NOT NULL,
+	countryID char(3) PRIMARY KEY
 );
 
 CREATE TABLE Favorites (
-	country char(3) REFERENCES Country(country) NOT NULL,
+	countryID char(3) REFERENCES Country(countryID) NOT NULL,
 	username VARCHAR(32) REFERENCES Users(username) NOT NULL,
-	PRIMARY KEY (country, username)
+	FOREIGN KEY (countryID, username)
 );
 
 CREATE TABLE Users (
@@ -45,10 +45,10 @@ CREATE TABLE Campaign (
 	name VARCHAR(32) NOT NULL,
 	description VARCHAR(64) NOT NULL,
 	paylink VARCHAR(64) NOT NULL,
-	creator VARCHAR(32) References User(username) NOT NULL
+	creator VARCHAR(32) REFERENCES User(username) NOT NULL
 );
 
 CREATE TABLE Likes (
-	username VARCHAR(32) PRIMARY KEY REFERENCES Users(username) NOT NULL,
+	username VARCHAR(32) FOREIGN KEY REFERENCES Users(username) NOT NULL,
 	CID uuid REFERENCES Campaign(CID) NOT NULL
 );
