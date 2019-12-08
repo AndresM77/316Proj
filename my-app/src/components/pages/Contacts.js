1a1665ef483bc50dd2580afa758012fb5a4ceea7
Andres Montoya-Aristizabal <andres.montoyaaristizabal@duke.edu>
2019-12-08 17:15:34 -0500

    created contacts page

--- my-app/src/components/pages/Contacts.js
+++ my-app/src/components/pages/Contacts.js
@@ -4,7 +4,19 @@ class Contacts extends Component {
   render() {
     return (
       <div>
-        <h2> Contacts page .. </h2>
+        <h2> The Team </h2>
+        <h3> Adam </h3>
+        <p> email: </p>
+        <h3> Andres </h3>
+        <p> email: am708@duke.edu </p>
+        <h3> Ethan </h3>
+        <p> email: </p>
+        <h3> Frank: </h3>
+        <p> email: </p>
+        <h3> Joeseph </h3>
+        <p> email: </p>
+        <h3> Ryan </h3>
+        <p> email: </p>
       </div>
     );
   }
