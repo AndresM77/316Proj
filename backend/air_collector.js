aaa21164e100a6b21b4391d4ecc1aeaa5cb1ecd0
Frank Geng <jianchao.geng@duke.edu>
2019-12-08 17:55:15 -0500

    added air collector

---
+++ backend/air_collector.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+const pool = require('./datastore/pgdb');
+const uuidv5 = require('uuid/v5');
+const MY_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
+pool.connect(function(err){
+    console.log("connected")
+    if(err)
+    {
+        console.log(err);
+    }
+});
+async function getMarker() {
+    let response = await fetch(`https://waqi.info/rtdata/`)
+    let data = await response.json();
+    return data.path;
+}
+async function getData() {
+    let marker = await getMarker()
+    let response = await fetch(`https://waqi.info/rtdata/${marker}/level1.json`);
+    let data = await response.json();
+    data.forEach(e => {
+        let lat =  e.g[0];
+        let long = e.g[1];
+        let value = parseFloat(e.a);
+        if(isNaN(value)) continue;
+        let dpid = uuidv5(String(lat)+String(long), MY_NAMESPACE);
+        pool.query(`INSERT INTO Air(DPID, lat, lng, countryID, quality)\
+                    VALUES($1, $2, $3, 'XXX', $4)`, [dpid, lat, long, value], (err) => {
+                        if(err) {
+                            console.log(err);
+                        }
+                    }
+        )       
+        console.log(e);
+    });
+}
+getData()
