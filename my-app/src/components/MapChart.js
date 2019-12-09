9da904b57e049acace1bf742df2a52d656eaae9c
Frank Geng <jianchao.geng@duke.edu>
2019-12-09 01:38:06 -0500

    fixed mapchart

--- my-app/src/components/MapChart.js
+++ my-app/src/components/MapChart.js
@@ -51,153 +51,123 @@ export default class MapChart extends React.Component {
 
   filterDataPoints = async() => {
     let points = []
-    if(this.state.selectedCategory == "air") {
-      points = await fetch(`http://localhost:3002/api/v1/dps/${this.state.selectedCategory}/2019`)
+    if(this.state.selectedCategory && this.state.selectedYear) {
+      points = await fetch(`https://frank.colab.duke.edu:3002/api/v1/dps/${this.state.selectedCategory}/${this.state.selectedYear}`)
       .catch(e => {
         console.log(e);
         return;
       })
-    }
-    else if (this.state.selectedCategory && this.state.selectedYear) {
-      points = await fetch(`http://frank.colab.duke.edu:3002/api/v1/dps/${this.state.selectedCategory}/${this.state.selectedYear}`)
-      .catch(e => {
-        console.log(e);
-        return;
-      })
-    } else {
-      return
     }
     console.log(this.state.selectedCategory, this.state.selectedYear);
     const json = points.length === 0 ? [] : await points.json();
     this.setState({selectedPoints: json});
-  };
-
-  render() {
-    let minColor;
-    let maxColor;
-    let minValue;
-    let maxValue;
-    if(this.state.selectedCategory === "air"){
-      minValue = 0;
-      maxValue = 800;
-      minColor = "#82ed86";
-      maxColor = "#CC0617";
-    } else if(this.state.selectedCategory === "temp"){
-      minValue = -20;
-      maxValue = 30;
-      minColor = "#CFD8DC";
-      maxColor = "#C94242";
-    } else {
-      minValue = 0
-      maxValue = 783
-      minColor = "#E66232";
-      maxColor = "#214ADE";
-    }
-
-    
-
-  const customScale = scaleLinear()
-  .domain([minValue, maxValue])
-  .range([minColor, maxColor]);
-
-    return (
-      <div>
-        <div style={{ position: "absolute", bottom: "10vh", left: "4vw", width: "300px" }}>
-          <DataTypeSelect handleCategoryChange={this.handleCategoryChange} />
-          <YearSlider
-            startYear={1901}
-            endYear={2016}
-            handleYearChange={this.handleYearChange}
-          />
-        </div>
+  }
+  
+    render() {
+      let minColor;
+      let maxColor;
+      if(this.state.selectedCategory === "air"){
+        minColor = "#82ed86";
+        maxColor = "#CC0617";
+      } else if(this.state.selectedCategory === "temp"){
+        minColor = "#CFD8DC";
+        maxColor = "#C94242";
+      } else {
+        minColor = "#E66232";
+        maxColor = "#214ADE";
+      }
+  
+      const minValue = 5; // based on the data array above
+      const maxValue = 20; // based on the data array above
+  
+    const customScale = scaleLinear()
+    .domain([minValue, maxValue])
+    .range([minColor, maxColor]);
+  
+      return (
         <div>
-          <ComposableMap
-            data-tip=""
-            projectionConfig={{ scale: 180 }}
-            width={980}
-            height={500}
-            style={{
-              width: "100%",
-              height: "auto"
-            }}
-          >
-            <ZoomableGroup zoom={1}>
-            <Geographies
-              geography={
-                "https://raw.githubusercontent.com/Frankgeng/react-simple-maps/master/topojson-maps/world-110m.json"
-              }
+          <div style={{ position: "absolute", bottom: "10vh", left: "4vw", width: "300px" }}>
+            <DataTypeSelect handleCategoryChange={this.handleCategoryChange} />
+            {this.state.selectedCategory !== "air" ? <YearSlider
+                startYear={1901}
+                endYear={2016}
+                handleYearChange={this.handleYearChange}
+              /> : <> </>}
+          </div>
+          <div>
+            <ComposableMap
+              data-tip=""
+              projectionConfig={{ scale: 180 }}
+              width={980}
+              height={500}
+              style={{
+                width: "100%",
+                height: "auto"
+              }}
             >
-              {({ geographies }) =>
-                geographies.map((geo, i) => {
-                  const country = this.state.selectedPoints.find(
-                    p => p.country === geo.properties.ISO_A3
-                  );
-                  let measurement;
-                  if (this.state.selectedCategory === "temp") measurement = "temperature";
-                  else if (this.state.selectedCategory === "air") {
-                    measurement = "quality";
-                  }
-                  else if (this.state.selectedCategory === "rain") measurement = "rainfall";       
-                  return (
-                    <Geography
-                      key={geo.properties.ISO_A3 + i}
-                      cacheId={geo.properties.ISO_A3 + i}
-                      geography={geo}
-                      onMouseEnter={() => {
-                        const { NAME } = geo.properties;
-                        
-                        if(country) this.props.setTooltipContent(this.handleTooltipContent(NAME, country[measurement]));
-                      }}
-                      onMouseLeave={() => {
-                        this.props.setTooltipContent("");
-                      }}
-                      style={{
-                        default: {
-                          fill: country
-                            ? this.customScale(country[measurement])
-                            : "#ECEFF1",
-                          stroke: "black",
-                          strokeWidth: 0.75,
-                          outline: "none"
-                        },
-                        hover: {
-                          fill: country
-                            ? this.customScale(country[measurement])
-                            : "#ECEFF1",
-                          stroke: "black",
-                          strokeWidth: 0.75,
-                          outline: "none"
-                        },
-                        pressed: {
-                          fill: country
-                            ? this.customScale(country[measurement])
-                            : "#ECEFF1",
-                          stroke: "black",
-                          strokeWidth: 0.75,
-                          outline: "none"
-                        }
-                      }}
-                    />
-                  );
-                })
-              }
-            </Geographies>
-            {this.state.selectedCategory === "air" ? this.state.selectedPoints.map((element, i) => {
-                    return(
-                      <Marker coordinates={[element.lng, element.lat]}
+              <ZoomableGroup zoom={1}>
+              <Geographies
+                geography={
+                  "https://raw.githubusercontent.com/Frankgeng/react-simple-maps/master/topojson-maps/world-110m.json"
+                }
+              >
+                {({ geographies }) =>
+                  geographies.map((geo, i) => {
+                    const country = this.state.selectedPoints.find(
+                      p => p.countryid === geo.properties.ISO_A3
+                    );
+                    let measurement;
+                    if (this.state.selectedCategory === "temp") measurement = "temperature";
+                    else if (this.state.selectedCategory === "air") measurement = "quality";
+                    else if (this.state.selectedCategory === "rain") measurement = "rainfall";        
+                    return (
+                      <Geography
+                        key={geo.properties.ISO_A3 + i}
+                        cacheId={geo.properties.ISO_A3 + i}
+                        geography={geo}
                         onMouseEnter={() => {
-                          this.props.setTooltipContent(`${element.quality} AQI`);
+                          const { NAME } = geo.properties;
+                          
+                          if(country) this.props.setTooltipContent(this.handleTooltipContent(NAME, country[measurement]));
                         }}
                         onMouseLeave={() => {
                           this.props.setTooltipContent("");
-                        }}>
-                        <circle r={2} fill="#F53" />
-                      </Marker>
-                    )}) : <> </>}
-          </ZoomableGroup>
-          </ComposableMap>
+                        }}
+                        style={{
+                          default: {
+                            fill: country
+                              ? customScale(country[measurement])
+                              : "#ECEFF1",
+                            stroke: "black",
+                            strokeWidth: 0.75,
+                            outline: "none"
+                          },
+                          hover: {
+                            fill: country
+                              ? customScale(country[measurement])
+                              : "#ECEFF1",
+                            stroke: "black",
+                            strokeWidth: 0.75,
+                            outline: "none"
+                          },
+                          pressed: {
+                            fill: country
+                              ? customScale(country[measurement])
+                              : "#ECEFF1",
+                            stroke: "black",
+                            strokeWidth: 0.75,
+                            outline: "none"
+                          }
+                        }}
+                      />
+                    );
+                  })
+                }
+              </Geographies>
+              </ZoomableGroup>
+            </ComposableMap>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
